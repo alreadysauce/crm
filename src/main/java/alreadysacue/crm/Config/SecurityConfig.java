66096c2b72e1bb0d30b2615bf2f911719e1d5dfc
alreadysauce <alreadysauce@gmail.com>
2022-07-07 23:50:07 +0900

    Join, logIn

---
+++ src/main/java/alreadysacue/crm/Config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package alreadysacue.crm.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity // fil
+public class SecurityConfig{
+    //해당되는 리턴되는 오브젝트를 IoC로 등록
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+        return http.build();
+    }
+}
--- src/main/java/alreadysacue/crm/Controller/IndexController.java
+++ src/main/java/alreadysacue/crm/Controller/IndexController.java
@@ -1,12 +1,49 @@
 package alreadysacue.crm.Controller;
 
+import alreadysacue.crm.Repository.UserRepository;
+import alreadysacue.crm.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+    @GetMapping("/loginForm")
+    public String login(){
+        return "/Login/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "/Login/joinForm";
+    }
+    @PostMapping("/join")
+    public @ResponseBody String join(User user) {
+        log.info(user.toString());
+        user.setRole("ROLE_ADMIN");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
     }
 }
